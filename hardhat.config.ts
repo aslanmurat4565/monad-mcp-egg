a59f4a335c2285d9a41897cd7ad5fed52bbc95ff
Satoshemedia <37773210+aslanmurat4565@users.noreply.github.com>
2025-04-30 10:22:21 +0300

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.0",
+  networks: {
+    monadTestnet: {
+      url: "https://testnet-rpc.monad.xyz",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+};
+
+export default config;
